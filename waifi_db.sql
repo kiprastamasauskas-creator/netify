4ba2988efc44be07cb547a29264ae735de47d423
kiprastamasauskas-creator <kipras.tamasauskas@gmail.com>
2026-08-25 09:16:24 +0200

    Integrate SQL database with application
    Integrated SQL database with the rest of the application.

--- waifi_db.sql
+++ waifi_db.sql
@@ -1,9 +1,1 @@
-USE waifi_db;
-
--- 1. Add the boolean flag to track verification status
--- ALTER TABLE users 
--- ADD COLUMN is_verified BOOLEAN DEFAULT FALSE;
-
--- 2. Add the column to store the temporary 6-digit code
--- ALTER TABLE users 
--- ADD COLUMN registration_otp VARCHAR(6);
+SQL database intagrated with the rest of the application.
